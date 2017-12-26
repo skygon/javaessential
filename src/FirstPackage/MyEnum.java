dc984056d49b288c51aad982d6ccd0a21aa5c913
skygon <skygoncui@163.com>
2017-12-26 09:47:25 +0800

    update

--- src/FirstPackage/MyEnum.java
+++ src/FirstPackage/MyEnum.java
@@ -5,10 +5,21 @@ public enum MyEnum {
 	ORANGE,
 	BANANA;
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws InstantiationException, IllegalAccessException {
+		// value
 		for (MyEnum e : MyEnum.values()) {
 			System.out.println(e.ordinal());
 			System.out.println(e.toString());
 		}
+		
+		// Object obj = new MyEnum(); //compile error. We can not create instance of enum
+		
+		// the class
+		Class<?> cls = MyEnum.APPLE.getClass();
+		System.out.println("MyEnum class: " + cls.getName());
+		
+		// What about create instance through class object
+		// Object obj = cls.newInstance();  // Run time error. InstantiationException will throw.
+		
 	}
 }
