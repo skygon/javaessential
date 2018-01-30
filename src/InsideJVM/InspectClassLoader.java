576d31243ea4cec0d472259180537ec3f5ce18de
yui <ycui@microstrategy.com>
2018-01-30 13:05:46 +0800

    update

--- src/InsideJVM/InspectClassLoader.java
+++ src/InsideJVM/InspectClassLoader.java
@@ -1,5 +1,18 @@
 package InsideJVM;
 
-public class InspectClassLoader {
+import java.net.URL;
 
+public class InspectClassLoader {
+	public void getResource(){
+		ClassLoader loader = getClass().getClassLoader();
+		System.out.println("Class loader is " + loader);
+		System.out.println(System.getProperty("java.class.path"));
+		URL res = loader.getResource("E:\\Java\\project\\javaessential\\resources\\mysql-connector.jar");
+		System.out.println("Find resource url is " + res);
+	}
+	
+	public static void main(String[] args){
+		InspectClassLoader ICL = new InspectClassLoader();
+		ICL.getResource();
+	}
 }
